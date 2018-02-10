8270e97a4b4e036030f0e1a5b947fe431fbad9e2
Emlembow <36314674+Emlembow@users.noreply.github.com>
2018-02-10 12:13:05 -0500

    Add files via upload

--- TollScrape.py
+++ TollScrape.py
@@ -1,5 +1,4 @@
 # Toll Scraper
-
 import time
 import datetime
 import csv
@@ -7,9 +6,6 @@ from selenium import webdriver
 
 
 def loadDriver():
-
-    # Open window and set Resolution
-    global driver
     driver = webdriver.Firefox()
     driver.set_window_position(0, 0)
     driver.set_window_size(400, 800)
@@ -17,43 +13,65 @@ def loadDriver():
 
 
 def whatTimeIsIt():
+    global east
+    global west
     global now
-    ts = time.time()
-    now = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    now = datetime.datetime.now()
+    east = ((now.isoweekday() in range(1, 6)) & (now.hour in range(5, 10)))
+    west = (now.weekday() < 5) & (now.hour in range(15, 19))
+
 
 def clickXPATH(XPATH):
     driver.find_element_by_xpath(XPATH).click()
     time.sleep(1)
 
-def run():
-    # Select Toll Route (Washington to 66-West)
-    clickXPATH("/html/body/form/div[4]/ul/li[1]/a/span[1]")
-    clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[4]/div[2]/span")
-    clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[6]/div/select/option[2]")
-    clickXPATH("//*[@id='btnUpdateBeginSel']")
-    clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[7]/div/select/option[6]")
-    clickXPATH("//*[@id='btnUpdateEndSel']")
-
-    # Scrape the Toll Amount and Append to CSV
-    var = 1
-    while var == 1:
-        clickXPATH("//*[@id='tollRefreshBtn']")
-        toll = driver.find_element_by_xpath("//*[@id='spanTollAmt']").text
-        whatTimeIsIt()
-        if toll[0] == "N":
-            toll = 0
-        with open('Output.csv', 'a') as newFile:
-            newFileWriter = csv.writer(newFile)
-            newFileWriter.writerow([now, toll])
-        print(toll, now)
-        print("CSV Updated")
-        time.sleep(60)
+def run(direction):
+    global commute
+    if direction is 'east':
+        commute = "Eastbound I66 West to Washington"
+        clickXPATH("/html/body/form/div[4]/ul/li[1]/a/span[1]") #Current Toll Estimate
+        clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[4]/div[1]/span") #Eastboound AM Tolls
+        clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[6]/div/select/option[2]") #Enter I-66West
+        clickXPATH("//*[@id='btnUpdateBeginSel']") #Select
+        clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[7]/div/select/option[8]") #Exit Washington
+        clickXPATH("//*[@id='btnUpdateEndSel']") #Select
+    elif direction is 'west':
+        commute = "Westbound Washington to I66West"
+        clickXPATH("/html/body/form/div[4]/ul/li[1]/a/span[1]")
+        clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[4]/div[2]/span")
+        clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[6]/div/select/option[2]")
+        clickXPATH("//*[@id='btnUpdateBeginSel']")
+        clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[7]/div/select/option[6]")
+        clickXPATH("//*[@id='btnUpdateEndSel']")
+    else:
+        print("[Error] direction '", direction, "' not supported")
+        driver.close()
+        driver.quit()
+    clickXPATH("//*[@id='tollRefreshBtn']")
+    toll = driver.find_element_by_xpath("//*[@id='spanTollAmt']").text
+    whatTimeIsIt()
+    if toll[0] == "N":
+        toll = 0
+    with open('Output.csv', 'a') as newFile:
+        newFileWriter = csv.writer(newFile)
+        newFileWriter.writerow([commute, now, toll])
+    print(commute, toll, now)
+    print("CSV Updated")
+    time.sleep(60)
 
 
 def main():
     if __name__ == "__main__":
         loadDriver()
-        run()
+        whatTimeIsIt()
+        while True:
+            if east is True:
+                run(east)
+            elif west is True:
+                run(west)
+            else:
+                print("No tolls right now. Waiting 60 seconds and trying again")
+                time.sleep(60)
 
 
 if __name__ == "__main__":
