6f0893c214de68162a6205c265d6293d0f27b7ba
Emlembow <36314674+Emlembow@users.noreply.github.com>
2018-02-10 14:23:52 -0500

    Add files via upload

--- TollScrape.py
+++ TollScrape.py
@@ -1,4 +1,5 @@
 # Toll Scraper
+
 import time
 import datetime
 import csv
@@ -10,7 +11,6 @@ def loadDriver():
     driver = webdriver.Firefox()
     driver.set_window_position(0, 0)
     driver.set_window_size(400, 800)
-    driver.get("https://vai66tolls.com/")
 
 
 def whatTimeIsIt():
@@ -18,54 +18,61 @@ def whatTimeIsIt():
     global west
     global now
     now = datetime.datetime.now()
-    east = ((now.isoweekday() in range(1, 6)) & (now.hour in range(5, 10)))
+    # east = ((now.isoweekday() in range(1, 6)) & (now.hour in range(5, 10)))
+    east = True
     west = (now.weekday() < 5) & (now.hour in range(15, 19))
 
 
-def clickXPATH(XPATH):
-    driver.find_element_by_xpath(XPATH).click()
+def clickxpath(xpath):
+    driver.find_element_by_xpath(xpath).click()
     time.sleep(1)
+
 
 def run(direction):
     global commute
     if direction is east:
         commute = "Eastbound I66 West to Washington"
-        clickXPATH("/html/body/form/div[4]/ul/li[1]/a/span[1]")  # Current Toll Estimate
-        clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[4]/div[1]/span")  # Eastbound AM Tolls
-        clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[6]/div/select/option[2]")  # Enter I-66West
-        clickXPATH("//*[@id='btnUpdateBeginSel']")  # Select
-        clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[7]/div/select/option[8]")  # Exit Washington
-        clickXPATH("//*[@id='btnUpdateEndSel']")  # Select
+        driver.get("https://vai66tolls.com/")
+        clickxpath("/html/body/form/div[4]/ul/li[1]/a/span[1]")  # Current Toll Estimate
+        clickxpath("/html/body/form/div[5]/div/div/div/div/div[2]/div[4]/div[1]/span")  # Eastbound AM Tolls
+        clickxpath("/html/body/form/div[5]/div/div/div/div/div[2]/div[6]/div/select/option[2]")  # Enter I-66West
+        clickxpath("//*[@id='btnUpdateBeginSel']")  # Select
+        clickxpath("/html/body/form/div[5]/div/div/div/div/div[2]/div[7]/div/select/option[8]")  # Exit Washington
+        clickxpath("//*[@id='btnUpdateEndSel']")  # Select
     elif direction is west:
         commute = "Westbound Washington to I66West"
-        clickXPATH("/html/body/form/div[4]/ul/li[1]/a/span[1]")  # Current Toll Estimate
-        clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[4]/div[2]/span")  # Westbound PM Tolls
-        clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[6]/div/select/option[2]")  # Enter Washington
-        clickXPATH("//*[@id='btnUpdateBeginSel']")  # Select
-        clickXPATH("/html/body/form/div[5]/div/div/div/div/div[2]/div[7]/div/select/option[6]")  # Exit I-66West
-        clickXPATH("//*[@id='btnUpdateEndSel']")  # Select
+        driver.get("https://vai66tolls.com/")
+        clickxpath("/html/body/form/div[4]/ul/li[1]/a/span[1]")  # Current Toll Estimate
+        clickxpath("/html/body/form/div[5]/div/div/div/div/div[2]/div[4]/div[2]/span")  # Westbound PM Tolls
+        clickxpath("/html/body/form/div[5]/div/div/div/div/div[2]/div[6]/div/select/option[2]")  # Enter Washington
+        clickxpath("//*[@id='btnUpdateBeginSel']")  # Select
+        clickxpath("/html/body/form/div[5]/div/div/div/div/div[2]/div[7]/div/select/option[6]")  # Exit I-66West
+        clickxpath("//*[@id='btnUpdateEndSel']")  # Select
     else:
         print("[Error] direction '", direction, "' not supported")
         driver.close()
         driver.quit()
-    clickXPATH("//*[@id='tollRefreshBtn']")
-    toll = driver.find_element_by_xpath("//*[@id='spanTollAmt']").text
+
+
+def collect():
     whatTimeIsIt()
+    clickxpath("//*[@id='tollRefreshBtn']")
+    toll = driver.find_element_by_xpath("//*[@id='spanTollAmt']").text
     if toll[0] == "N":
         toll = 0
     with open('Output.csv', 'a') as newFile:
-        newFileWriter = csv.writer(newFile)
-        newFileWriter.writerow([commute, now, toll])
+        newfilewriter = csv.writer(newFile)
+        newfilewriter.writerow([commute, now, toll])
     print(commute, toll, now)
-    print("CSV Updated")
+    print("CSV Updated\n")
     time.sleep(60)
 
 
 def main():
     if __name__ == "__main__":
         loadDriver()
-        whatTimeIsIt()
         while True:
+            whatTimeIsIt()
             if east is True:
                 run(east)
             elif west is True:
@@ -73,6 +80,7 @@ def main():
             else:
                 print("No tolls right now. Waiting 60 seconds and trying again")
                 time.sleep(60)
+            collect()
 
 
 if __name__ == "__main__":
